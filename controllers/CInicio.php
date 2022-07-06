1384c5cf2fb498082f8daf48a6182b6d246cafb3
Nelly-94 <108025988+Nelly-94@users.noreply.github.com>
2022-07-06 15:15:31 -0500

    Add files via upload

---
+++ controllers/CInicio.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+**/
+class Cinicio extends CI_Controller 
+{
+    function _construct()
+        {
+            parent::__construct();
+        }
+        public function index() {
+            $this->load->view('inicio');
+        }
+}
+?>
---
+++ controllers/CRegistro.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ */
+class Cregistro extends CI_Controller
+{
+    function _construct()
+    {
+        parent::__construct();
+    }
+    public function index(){
+        $this->load->view('registro');
+    }
+}  
+?>
